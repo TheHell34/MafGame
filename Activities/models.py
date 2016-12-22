2c0b923811d98af549950bc417e329c5ed7ff3dd
Asphyxi <drvh15@gmail.com>
2016-12-22 13:35:34 +0100

    fast fixes

--- Activities/models.py
+++ Activities/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
-class Activity(models.Model):
+
+class activity(models.Model):
     activity_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     cost = models.BigIntegerField()
--- Activities/tests.py
+++ Activities/tests.py
@@ -1,4 +1,12 @@
 from django.test import TestCase
+from Player.models import player
+from Activities.models import activity
 
-
-
+class ActivityTestCase(TestCase):
+    def setuptest(self):
+        player.objects.create(name="Do", password="test123", money=500000, xp=0, health=100, armor=100, registered="2016-12-22")
+        activity.objects.create(name="RPS", cost=50, reward=100)
+    def Test_Give_Reward(self):
+        x = player.objects.get(name="Do")
+        y = activity.objects.get(name="RPS")
+        self.assertEqual(y.give_reward(x), 0)

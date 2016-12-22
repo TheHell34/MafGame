7e15c5b42cc444864bff180ed78637b00ba09b8d
Asphyxi <drvh15@gmail.com>
2016-12-22 13:27:00 +0100

    Skeletons

--- Activities/admin.py
+++ Activities/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from Activities.models import Activity
 
 # Register your models here.
+
+admin.site.register(Activity)
--- Activities/models.py
+++ Activities/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Activity(models.Model):
+    activity_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    cost = models.BigIntegerField()
+    reward = models.BigIntegerField()
+
+    def give_reward(self, player):
+        player.money += self.reward
+        player.save()
+        return player.money

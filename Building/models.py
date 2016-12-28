10bf2ad07767571d9b42c53082692b5ad5db76ec
TheHell34 <mnachtigaal@outlook.com>
2016-12-28 14:00:33 +0100

    added building functions and test

--- Building/models.py
+++ Building/models.py
@@ -1,7 +1,10 @@
 from django.db import models
-
+from Player.models import player
 
 # Create your models here.
+
+
+
 class building(models.Model):
     building_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
@@ -11,6 +14,43 @@ class building(models.Model):
     requiredxp = models.BigIntegerField()
 
     def buy(self, player):
-        player.money = player.money - self.cost
+        player.money -= self.cost
+        self.register_building(player)
         player.save()
-        return player.money
+
+    def register_building(self, p):
+        player_building.objects.create(player_id=p, building_id=self, level=1, perhour=self.perhour, cost=self.cost * self.multiplier)
+        pb = player_building.objects.get(player_id=p, building_id=self)
+        pb.save()
+
+    def generate_money(self, player):
+        player.money += self.perhour
+        player.save()
+
+    def upgrade(self, player):
+        pb = player_building.objects.get(player_id=player, building_id=self)
+        if player.money >= pb.cost:
+            player.money -= pb.cost
+            pb.level += 1
+            pb.cost *= self.multiplier
+            pb.perhour += self.perhour
+            player.save()
+            pb.save()
+        else:
+            return False
+
+
+
+class player_building(models.Model):
+    id = models.AutoField(primary_key=True)
+    player_id = models.ForeignKey(player)
+    building_id = models.ForeignKey(building)
+    level = models.IntegerField()
+    perhour = models.BigIntegerField()
+    cost = models.BigIntegerField()
+
+
+
+
+
+
--- Building/tests.py
+++ Building/tests.py
@@ -4,14 +4,41 @@ from django.conf import settings
 # Create your tests here.
 from Building.models import building
 from Player.models import player
+from Building.models import player_building
 
 
 class BuildingTestCase(TestCase):
     def setUp(self):
         building.objects.create(name="fabriek", perhour=1, cost=100, multiplier=1.1, requiredxp=0)
-        player.objects.create(name="Mike", password="Kaas", money=100, xp=0, health=100, armor=100, registered="2012-09-28T16:41:12.9976565")
+        player.objects.create(name="Mike", password="Kaas", money=1000, xp=0, health=100, armor=100, registered="2012-09-28T16:41:12.9976565")
 
     def test_building_buy(self):
         b = building.objects.get(name="fabriek")
         p = player.objects.get(name="Mike")
-        self.assertEqual(b.buy(p), 0)
+        b.buy(p)
+        self.assertEqual(p.money, 900)
+
+    def test_generate_money(self):
+        b = building.objects.get(name="fabriek")
+        p = player.objects.get(name="Mike")
+        b.generate_money(p)
+        self.assertEqual(p.money, 1001)
+
+    def test_upgrade(self):
+        b = building.objects.get(name="fabriek")
+        p = player.objects.get(name="Mike")
+        b.buy(p)
+        b.upgrade(p)
+        pb = player_building.objects.get(player_id=p.player_id, building_id=b.building_id)
+        self.assertEqual(p.money, 790)
+        self.assertEqual(pb.cost, 121)
+
+    def test_upgrade_mutiple(self):
+        b = building.objects.get(name="fabriek")
+        p = player.objects.get(name="Mike")
+        b.buy(p)
+        for i in range(0, 4):
+            b.upgrade(p)
+        pb = player_building.objects.get(player_id=p.player_id, building_id=b.building_id)
+        self.assertEqual(p.money, 390)
+        self.assertEqual(pb.cost, 160)
